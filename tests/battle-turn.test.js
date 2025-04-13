d0f7c84b42c7e94f95787040eb75477d551e281e
tadosuke <tadosuke@gmail.com>
2025-04-13 23:11:49 +0900

    コード整理

--- tests/battle-turn.test.js
+++ tests/battle-turn.test.js
@@ -6,6 +6,7 @@ describe("constructor", () => {
     test("渡した引数がメンバーに設定されているか？", () => {
         const party1 = new Party("party1", [new Character("character1")]);
         const party2 = new Party("party2", [new Character("character2")]);
+
         const turn = new BattleTurn(1, party1, party2);
 
         expect(turn.party1).toBe(party1);
@@ -30,11 +31,10 @@ describe("getCharactersBySpeed", () => {
         const character1 = new Character("character1", 100, 10, 10, 10); // speed: 10
         const character2 = new Character("character2", 100, 10, 10, 20); // speed: 20
         const character3 = new Character("character3", 100, 10, 10, 15); // speed: 15
-
         const party1 = new Party("party1", [character1, character3]);
         const party2 = new Party("party2", [character2]);
-
         const battleTurn = new BattleTurn(1, party1, party2);
+
         const sortedCharacters = battleTurn.getCharactersBySpeed();
 
         expect(sortedCharacters).toEqual([character2, character3, character1]);
