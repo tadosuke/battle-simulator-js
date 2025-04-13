68b07d570408d1d5ce5b3604acc1ed56aae9c17c
tadosuke <tadosuke@gmail.com>
2025-04-13 18:44:58 +0900

    初期ファイルを削除

--- tests/hello.test.js
+++
@@ -1,9 +0,0 @@
-
-import { hello } from "../src/hello";
-import { describe, test, expect } from "vitest";
-
-describe("hello", () => {
-    test("should return 'Hello, world!'", () => {
-        expect(hello()).toBe("Hello, world!");
-    });
-});
