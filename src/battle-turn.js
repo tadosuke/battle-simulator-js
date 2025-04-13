bb03afb75b3f51afb947948203aeb8b10908f848
tadosuke <tadosuke@gmail.com>
2025-04-13 20:14:50 +0900

    ターンのテストを追加

--- src/battle-turn.js
+++ src/battle-turn.js
@@ -1,11 +1,19 @@
 import Party from "./party.js";
 
+// 実行結果
+export const TURN_RESULT = {
+    CONTINUE: 0, // 継続中
+    PARTY1_WON: 1, // パーティ1の勝利
+    PARTY2_WON: 2, // パーティ2の勝利
+    DRAW: 3, // 引き分け
+};
+
 /**
  * 戦闘における1ターンを表すクラス
  * @param {Party} party1 - 戦闘に参加するパーティ1
  * @param {Party} party2 - 戦闘に参加するパーティ2
  */
-export default class BattleTurn {
+export class BattleTurn {
     constructor(party1, party2) {
         this.party1 = party1;
         this.party2 = party2;
@@ -13,6 +21,9 @@ export default class BattleTurn {
 
     /**
      * ターンを実行する
+     * @returns 戦闘の結果（TURN_RESULT）
      */
-    execute() {}
+    execute() {
+        return TURN_RESULT.CONTINUE;
+    }
 }
--- tests/battle-turn.test.js
+++ tests/battle-turn.test.js
@@ -1,5 +1,22 @@
-import BattleTurn from "../src/battle-turn";
+import * as battleTurn from "../src/battle-turn";
+import Character from "../src/character";
+import Party from "../src/party";
 
 describe("BattleTurn", () => {
-    test("コンストラクタで渡した引数がメンバーに設定されているか？", () => {});
+    test("コンストラクタで渡した引数がメンバーに設定されているか？", () => {
+        const party1 = new Party("party1", [new Character("character1")]);
+        const party2 = new Party("party2", [new Character("character2")]);
+        const turn = new battleTurn.BattleTurn(party1, party2);
+
+        expect(turn.party1).toBe(party1);
+        expect(turn.party2).toBe(party2);
+    });
+
+    test("executeメソッドがTURN_RESULT.CONTINUEを返すか？", () => {
+        const party1 = new Party("party1", [new Character("character1")]);
+        const party2 = new Party("party2", [new Character("character2")]);
+        const turn = new battleTurn.BattleTurn(party1, party2);
+
+        expect(turn.execute()).toBe(battleTurn.TURN_RESULT.CONTINUE);
+    });
 });
